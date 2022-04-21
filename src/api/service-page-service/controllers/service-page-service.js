3c7f53d2789b2364ad0f3c3c49d5573092b7ad69
Muhammad Farhan <45181106+farsidd@users.noreply.github.com>
2022-04-21 18:16:10 +0500

    services names and icons added

---
+++ src/api/service-page-service/controllers/service-page-service.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  service-page-service controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::service-page-service.service-page-service');
---
+++ src/api/service-page-service/routes/service-page-service.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * service-page-service router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::service-page-service.service-page-service');
