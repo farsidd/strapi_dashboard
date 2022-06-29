eb18626b5a81084b0099d657e78f8b54ce340e43
Muhammad Farhan <farhan@sendplex.com>
2022-06-30 03:10:04 +0500

    product page gallery images section changed

---
+++ src/api/product-category-name/routes/product-category-name.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-category-name router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::product-category-name.product-category-name');
---
+++ src/api/product-category-name/services/product-category-name.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-category-name service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::product-category-name.product-category-name');
